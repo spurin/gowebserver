f792a1106ea1e44c81b2549b021ce9618006b7fa
James Spurin <James.Spurin@reponixio.com>
2015-08-01 18:44:53 +0100

    formatting changes

--- src/gowebserver/gowebserver.go
+++ src/gowebserver/gowebserver.go
@@ -9,6 +9,7 @@ import (
 var port = flag.String("port", "8080", "TCP port to listen on")
 var root = flag.String("root", ".", "Filesystem root path")
 
+
 func main() {
 	flag.Parse()
     fmt.Println("James Spurin's mini go webserver - listening on port:", *port, "serving files from location:", *root)
